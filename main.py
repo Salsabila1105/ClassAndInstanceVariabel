b1096437aacea31f5575ecf9e3528568570bf253
Salsabila1105 <75847941+Salsabila1105@users.noreply.github.com>
2022-01-08 23:12:44 +0000

    variabel

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 class Ricecooker:
+  jumlah = 0
   def __init__(self,nama_merk,tuas,mode): 
     self.nama_merk = nama_merk
     self.tuas = tuas 
     self.mode = mode
+    Ricecooker.jumlah += 1
+    print('jumlah mode ricecooker :' + self.mode)
 
 class TypeRicecooker:
   def __init__(self,temperature):
@@ -20,3 +23,12 @@ print(YoungMa.nama_merk)
 print(YoungMa.tuas)
 print(YoungMa.mode)
 print(YoungMa.temperature)
+print(Ricecooker.jumlah)
+Cosmo= ModernRicecooker('Cosmo', 'OFF', 'Warm', 70)
+print(Cosmo.nama_merk)
+print(Cosmo.tuas)
+print(Cosmo.mode)
+print(Cosmo.temperature)
+print(Ricecooker.jumlah)
+
+
